9d78ad835d209c779666f56bfdf869721b51f4ce
amzaloga <30347270+amzaloga@users.noreply.github.com>
2020-11-13 20:48:40 -0500

    This is the HTTP call to get data via APIs. Still need to find good API

--- Main.java
+++ Main.java
@@ -1,3 +1,17 @@
-**Hello from GitHub
+import java.net.*;
+import java.io.*;
 
-**Hello from Eclipse $
+public class Main {
+    public static void main(String[] args) throws Exception {
+        URL yahoo = new URL("https://api.weather.gov/points/39.7456,-97.0892");
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader(
+                                new InputStreamReader(
+                                yc.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null) 
+            System.out.println(inputLine);
+        in.close();
+    }
+}
